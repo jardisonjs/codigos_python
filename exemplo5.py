a682784e3ea6049f0dd2cba9e935843f1a28584e
js e mohamed <jardisonsantos5678@gmail.com>
2024-09-03 17:16:00 -0300

    Atividade 2

---
+++ exemplo5.py
@@ -0,0 +1,7 @@
+fz = input("informe o nome da sua fazenda: ")
+cav = float(input("informe quantos cavalos você tem: "))
+
+fer = cav*4
+
+
+print(f"A fazenda {fz} possui {cav} cavalos, a quantidade total de ferradura para equipar todos os cavalos é de {fer}.")
